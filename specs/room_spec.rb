73a39c2a7010d7c2a2d4d21472e608b505c609bd
StuartCharters <charters.stuart@gmail.com>
2018-06-01 10:50:26 +0100

    setup room spec

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -3,4 +3,12 @@ require_relative("../room.rb")
 
 class RoomTest < MiniTest::Test
 
+  def setup
+    @room1 = Room.new(10.00)
+  end
+
+  def test_room_has_price
+    assert_equal(10.00, @room1.price())
+  end
+
 end
