93144cf673427f3eeb41b21bc46defda1356977b
StuartCharters <charters.stuart@gmail.com>
2018-06-01 10:44:13 +0100

    set up song

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -3,4 +3,12 @@ require_relative("../song.rb")
 
 class SongTest < MiniTest::Test
 
+  def setup
+    @song1 = Song.new("Make a man out of you")
+  end
+
+  def test_song_has_name
+    assert_equal("Make a man out of you", @song1.name())
+  end
+
 end
