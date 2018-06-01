7ff8810a6c1abd64c454b2e638d34f9b0fcebb5a
StuartCharters <charters.stuart@gmail.com>
2018-06-01 11:06:50 +0100

    setup add guest and many guest tests

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -1,6 +1,7 @@
 require("minitest/autorun")
 require_relative("../room.rb")
 require_relative("../song.rb")
+require_relative("../guest.rb")
 
 class RoomTest < MiniTest::Test
 
@@ -8,7 +9,10 @@ class RoomTest < MiniTest::Test
     @room1 = Room.new(10.00)
     @song1 = Song.new("Make a man out of you")
     @song2 = Song.new("Once upon a Decomber")
+    @guest1 = Guest.new("Stuart")
+    @guest2 = Guest.new("Sophie")
     @songs1 = [@song1, @song2]
+    @guests1 = [@guest1, @guest2]
   end
 
   def test_room_has_price
@@ -23,6 +27,16 @@ class RoomTest < MiniTest::Test
   def test_add_many_songs
     @room1.add_many_songs(@songs1)
     assert_equal(2, @room1.songs().length())
+  end
+
+  def test_add_guest
+    @room1.add_guest(@guest1)
+    assert_equal(1, @room1.occupants().length())
+  end
+
+  def test_add_many_guests
+    @room1.add_many_guests(@guests1)
+    assert_equal(2, @room1.occupants().length())
   end
 
 end
