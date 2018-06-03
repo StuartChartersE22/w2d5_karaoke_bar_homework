edb28abc7f0660bb944e44a11e6bc3cc4ebc4885
StuartCharters <charters.stuart@gmail.com>
2018-06-03 12:54:35 +0100

    corrected reception_spec, all tests passing for all classes

--- specs/reception_spec.rb
+++ specs/reception_spec.rb
@@ -2,6 +2,7 @@ require("minitest/autorun")
 require_relative("../club.rb")
 require_relative("../guest.rb")
 require_relative("../song.rb")
+require_relative("../karaoke_room.rb")
 require_relative("../reception.rb")
 
 class ReceptionTest < MiniTest::Test
@@ -18,46 +19,46 @@ class ReceptionTest < MiniTest::Test
     @guest3 = Guest.new("Joe", 20.00)
     @party1 = [@guest1, @guest2]
 
-    @club1 = Club.new("Da Mic", rooms)
+    club1 = Club.new("Da Mic", rooms)
 
   end
 
   def test_add_to_till
     @reception.add_cash(10.00)
-    assert_equal(10.00, reception.till())
+    assert_equal(10.00, @reception.till())
   end
 
   def test_admit_guest_to_room__sufficient_space_and_funds
     @reception.admit_guest_to_room("fairytale", @guest1)
     assert_equal(4, @room1.number_of_spaces_left())
     assert_equal(40.00, @guest1.wallet())
-    assert_equal(10.00, @club1.till())
+    assert_equal(10.00, @reception.till())
     assert_equal(@room1, @guest1.in_room())
   end
 
   def test_admit_guest_to_room__insufficient_space_and_sufficient_funds
     @room2.add_party(@party1)
-    @club1.admit_guest_to_room("musical", @guest3)
+    @reception.admit_guest_to_room("musical", @guest3)
     assert_equal(0, @room2.number_of_spaces_left())
     assert_equal(20.00, @guest3.wallet())
-    assert_equal(0.00, @club1.till())
+    assert_equal(0.00, @reception.till())
   end
 
   def test_admit_guest_to_room__sufficient_space_and_insufficient_funds
     @room1.add_party(@party1)
     @guest3.remove_cash(15.50)
-    @club1.admit_guest_to_room("fairytale", @guest3)
+    @reception.admit_guest_to_room("fairytale", @guest3)
     assert_equal(3, @room1.number_of_spaces_left())
     assert_equal(4.50, @guest3.wallet())
-    assert_equal(0.00, @club1.till())
+    assert_equal(0.00, @reception.till())
   end
 
   def test_admit_party_to_room__sufficient_space_and_funds
-    @club1.admit_party_to_room("fairytale", @party1)
+    @reception.admit_party_to_room("fairytale", @party1)
     assert_equal(3, @room1.number_of_spaces_left())
     assert_equal(40.00, @guest1.wallet())
     assert_equal(10.00, @guest2.wallet())
-    assert_equal(20.00, @club1.till())
+    assert_equal(20.00, @reception.till())
     assert_equal(@room1, @guest1.in_room())
     assert_equal(@room1, @guest2.in_room())
   end
@@ -65,22 +66,22 @@ class ReceptionTest < MiniTest::Test
   def test_admit_party_to_room__sufficient_space_and_insufficient_funds
     @guest3.remove_cash(15.50)
     party2 = @party1.push(@guest3)
-    @club1.admit_party_to_room("fairytale", party2)
+    @reception.admit_party_to_room("fairytale", party2)
     assert_equal(5, @room1.number_of_spaces_left())
     assert_equal(50.00, @guest1.wallet())
     assert_equal(20.00, @guest2.wallet())
     assert_equal(4.50, @guest3.wallet())
-    assert_equal(0.00, @club1.till())
+    assert_equal(0.00, @reception.till())
   end
 
   def test_admit_party_to_room__insufficient_space_and_sufficient_funds
     party2 = @party1.push(@guest3)
-    @club1.admit_party_to_room("musical", party2)
+    @reception.admit_party_to_room("musical", party2)
     assert_equal(2, @room2.number_of_spaces_left())
     assert_equal(50.00, @guest1.wallet())
     assert_equal(20.00, @guest2.wallet())
     assert_equal(20.00, @guest3.wallet())
-    assert_equal(0.00, @club1.till())
+    assert_equal(0.00, @reception.till())
   end
 
 end
